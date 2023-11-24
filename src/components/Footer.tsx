9e040774fba0742b5eef53c19420a9adae90159d
miftt <miftahfauzi012@gmail.com>
2023-11-24 11:24:37 +0700

    fixing image profile and fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,11 @@
 const Footer = () => {
-    return ( 
-        <div>
-            Footer
-        </div>
-     );
-}
- 
+  return (
+    <div>
+      <div className="flex rounded-t-[18px] items-center text-lg justify-center mt-4 bg-slate-100 h-12 w-full">
+        <p className="">Mifuzi</p>
+      </div>
+    </div>
+  );
+};
+
 export default Footer;
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,30 +1,32 @@
-// Profile.js
-import React from 'react';
-import Image from 'next/image';
+"use client";
 
-import profilePict from "@/images/profile.jpg"
+import React from "react";
+import Image from "next/image";
 
+import profilePict from "../../public/images/Profile.jpg";
 
 const Profile = () => {
   return (
     <div className="bg-slate-50 p-12 rounded-xl h-full w-3/5">
-  <div className='flex justify-center bg-slate-7'>
-    <div className="bg-black justify-center items-center rounded-full pb-4 w-12 h-12 overflow-hidden">
-      <Image
-        src={profilePict}
-        width={100}
-        height={100}
-        alt="Profile Picture"
-        objectFit="cover"
-        objectPosition="center"
-      />
+      <div className="flex justify-center bg-slate-7">
+        <div className="flex justify-center items-center rounded-full w-32 h-32 object-cover object-center overflow-hidden">
+          <Image
+            src={profilePict}
+            width={250}
+            height={250}
+            objectFit="cover"
+            objectPosition="center bottom"
+            alt="Profile Picture"
+          />
+        </div>
+      </div>
+      <p className="pt-4 text-4xl font-semibold">Miftah Fauzi</p>
+      <p className="text-lg">Welcome to my Portfolio Website</p>
+      <p className="text-lg">
+        I’m a student from Pasundan University and i live in Bandung, Jawa
+        Barat, Indonesia.
+      </p>
     </div>
-  </div>
-  <p className="text-3xl font-semibold">Miftah Fauzi</p>
-  <p className="text-sm">Welcome to my Portfolio Website</p>
-  <p className="text-sm">I’m a student from Pasundan University and i live in Bandung, Jawa Barat, Indonesia.</p>
-</div>
-
   );
 };
 
